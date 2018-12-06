86383edf7a4643d4e79885cc48dcc1514b89f26b
Theo Santos <theosnake01@gmail.com>
2018-12-06 17:30:23 +0100

    Images for different kind of food

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,9 @@ function getFood(){
     for (let i = 0; i < response.data.objects.length; i++) {
       content += `<div class="col-lg-4 col-md-6 mb-4">
       <div class="card h-100">
-      <a href="#" onclick="" id="` + response.data.objects[i].id + `"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+      <div class="card-header">
+      <a href="#" onclick="" id="` + response.data.objects[i].id + `"><img class="card-img-top" src="`+ getTypeImage(response.data.objects[i].type)+ `" alt=""></a>
+      </div>
       <div class="card-body">
       <h4 class="card-title" id="` + response.data.objects[i].id + `">`+ response.data.objects[i].nom +`</h4>
       <h5 class="card-title">`+ response.data.objects[i].prix + `€</h5>
@@ -35,6 +37,19 @@ function getType(type)
     return "Dessert";
   } else if (type == 4) {
     return "Boisson";
+  }
+}
+
+function getTypeImage(type)
+{
+  if (type == 1) {
+    return "media/entree_item.png";
+  } else if (type == 2) {
+    return "media/plat_item.png";
+  } else if (type == 3) {
+    return "media/dessert_item.png";
+  } else if (type == 4) {
+    return "media/boisson_item.png";
   }
 }
 
